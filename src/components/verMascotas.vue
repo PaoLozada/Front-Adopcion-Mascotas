98acde6235fa7c5a394cd2634b44e9ae96df2005
Plozada <plozada>
2021-10-18 23:25:49 -0500

    make it better

--- src/components/verMascotas.vue
+++ src/components/verMascotas.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="verMascotas">
+        <br>
+        <br>
         <h2>Bienvenido a nuestra galería</h2>
-        <br>
-        <br>
         <div class="contenedorMascotas">
             
             <div class = "caja_mascota">
@@ -40,41 +40,7 @@
                 <h3> Nombre: Pola</h3>
                 <h3> Edad: 14 meses</h3>
             </div>
-             <div class = "caja_mascota">
-                <img src= "https://planetamascotas.files.wordpress.com/2010/06/criollo-blog.jpg" width="150" height="150">
-                <h3> Nombre: Tomas</h3>
-                <h3> Edad: 3 meses</h3>
-            </div>
-            <div class = "caja_mascota">
-                <img src= "	https://i.pinimg.com/originals/1d/15/4f/1d154fa83507b3a127433cffc9c76eaa.jpg" width="150" height="150">
-                <h3> Nombre: Sasy</h3>
-                <h3> Edad: 4 meses</h3>
-            </div>
-            <div class = "caja_mascota">
-                <img src= "https://www.elcolombiano.com/blogs/dametupata/wp-content/uploads/2012/01/annie.jpg" width="150" height="150">
-                <h3> Nombre: Lili</h3>
-                <h3> Edad: 3 meses</h3>
-            </div>
-            <div class = "caja_mascota">
-                <img src= "https://pbs.twimg.com/media/EZIIlt6XYAIZ27p.jpg"  width="150" height="150">
-                <h3> Nombre: Pili</h3>
-                <h3> Edad: 3 meses</h3>
-            </div>
-            <div class = "caja_mascota">
-                <img src= "	https://www.elcolombiano.com/blogs/dametupata/wp-content/uploads/2012/01/pelusa.jpg"  width="150" height="150">
-                <h3> Nombre: Tato</h3>
-                <h3> Edad: 5 meses</h3>
-            </div>
-            <div class = "caja_mascota">
-                <img src= "https://sumedico.blob.core.windows.net/images/2020/01/22/perroscriollos.jpg" width="150" height="150">
-                <h3> Nombre: Lucas</h3>
-                <h3> Edad: 12 meses</h3>
-            </div>
-            <div class = "caja_mascota">
-                <img src= "https://www.adopta.mx/wp-content/uploads/2013/05/criollo.jpg"  width="150" height="150">
-                <h3> Nombre: Pola</h3>
-                <h3> Edad: 14 meses</h3>
-            </div>
+             
             
         </div>
     </div>
@@ -101,7 +67,7 @@
         flex-wrap: wrap;
         display: flex;
         justify-content: space-evenly;
-        align-items: flex-start;
+        align-items: flex-end;
         height: 100vh;
       
     }
@@ -110,10 +76,13 @@
         font-size: 30px;
         text-align: center;
         margin: 0%;
+        padding: 20;
     }
     .verMascotas h3{
         color: #fff;
         font-size: 20px;
+        margin: 0%;
+        padding: 0%;
     }
     .caja_mascota{
         
