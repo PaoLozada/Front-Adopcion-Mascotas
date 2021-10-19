1cd8f061d9120c29cc2dc04cbaa65148be58f04e
Plozada <plozada>
2021-10-18 22:29:37 -0500

    make it better

--- src/components/verMascotas.vue
+++ src/components/verMascotas.vue
@@ -7,6 +7,16 @@
                 <h3> Nombre: Tomas</h3>
                 <h3> Edad: 3 meses</h3>
             </div>
+            <div class = "caja_mascota">
+                <img src= "https://www.pngitem.com/pimgs/m/266-2666091_cachorros-de-razas-grandes-hd-png-download.png" width="200" height="200">
+                <h3> Nombre: Sasy</h3>
+                <h3> Edad: 4 meses</h3>
+            </div>
+            <div class = "caja_mascota">
+                <img src= "https://www.pngitem.com/pimgs/m/266-2666091_cachorros-de-razas-grandes-hd-png-download.png" width="200" height="200">
+                <h3> Nombre: Lili</h3>
+                <h3> Edad: 3 meses</h3>
+            </div>
         </div>
     </div>
 </template>
@@ -26,6 +36,20 @@
         align-items: flex-start;
         
         
+    }
+    .contenedorMascotas{
+        margin: 0;
+        padding: 0%;
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+      
+    }
+    .verMascotas h2{
+        color: #fff;
+    }
+    .verMascotas h3{
+        color: #fff;
     }
     
 
