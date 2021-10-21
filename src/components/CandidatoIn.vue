f1bedfbd0aa7e6ae0531c04c15ee4a56babfc087
Plozada <plozada>
2021-10-20 20:34:53 -0500

    make it better

--- src/components/CandidatoIn.vue
+++ src/components/CandidatoIn.vue
@@ -2,27 +2,59 @@
     <div class="candidatoIn_user">
         <div class="container_CandidatoIn_candidato">
             <h2>Diligenciar Encuesta</h2>
+                <div class = "Encuesta">
+                    <div class = "DatosPersonales">
 
-            <form v-on:submit.prevent="processCandidatoIn" >
-                <input type="text" v-model="candidatos.Numero_Identificacion" placeholder="Numero_Identificacion">
-                <br>
-                
-                <input type="text" v-model="candidatos.Nombre_Completo" placeholder="Nombre_Completo">
-                <br>
-                
-                <input type="text" v-model="candidatos.Direccion" placeholder="Direccion">
-                <br>
-                
-                <input type="text" v-model="candidatos.Numero_Contacto" placeholder="Numero_Contacto">
-                <br>
+                        <form v-on:submit.prevent="processCandidatoIn" >
+                            <label for="Número de Identificación">Username</label><input type="text" v-model="candidatos.Numero_Identificacion" >
+                            <br>
+                            
+                            <label for="Nombres y Apellidos">Username</label><input type="text" v-model="candidatos.Nombre_Completo" >
+                            <br>
+                            
+                            <label for="Dirección">Username</label><input type="text" v-model="candidatos.Direccion" >
+                            <br>
+                            
+                            <label for="Númmero de Contacto">Username</label><input type="text" v-model="candidatos.Numero_Contacto" >
+                            <br>
 
-                <input type="email" v-model="candidatos.Email" placeholder="Email">
-                <br>
+                            <label for="Email">Username</label><input type="email" v-model="candidatos.Email">
+                            <br>
 
-                <input type="number" v-model="candidatos.Edad" placeholder="Edad">
-                
-                <button type="submit">Enviar Informacón</button>
-            </form>
+                            <label for="Edad">Username</label><input type="number" v-model="candidatos.Edad" >
+               
+                            
+                        </form>
+                </div>
+                <div class = "Complementarios">
+
+                        <form v-on:submit.prevent="processCandidatoIn" >
+                            <label for="Has tenido o tienes mascota">Username</label><input type="text" v-model="candidatos.Has_Tenido_Mascotas" >
+                            <br>
+                            
+                            <label for="¿Podrás ser un adoptante responsable?">Username</label><input type="text" v-model="candidatos.Seras_Responsable" >
+                            <br>
+                            
+                            <label for="¿Tienes un espacio adecuado para tener una mascota?">Username</label><input type="text" v-model="candidatos.Tienes_Espacio" >
+                            <br>
+                            
+                            <label for="¿Cuentas con tiempo suficiente ">Username</label><input type="text" v-model="candidatos.Tienes_Tiempo">
+                            <br>
+
+                            <label for="¿Puedes permitirte económicamente una adopción?">Username</label><input type="email" v-model="candidatos.Recursos_Economicos" >
+                            <br>
+
+                            <label for="Estás preparado para los problemas que puedan ocasionar?">Username</label><input type="number" v-model="candidatos.Afrontar_Problemas">
+                            <br>
+
+                            
+                        </form>
+                </div>
+
+
+            </div>
+            <button type="submit">Enviar Informacón</button>
+
         </div>
 
     </div>
@@ -43,7 +75,13 @@ export default {
                 Direccion: "",
                 Numero_Contacto: "",
                 Email: "",
-                Edad: 0                
+                Edad: 0,
+                Has_Tenido_Mascotas:"", 
+                Seras_Responsable:"", 
+                Tienes_Espacio:"", 
+                Tienes_Tiempo:"",
+                Recursos_Economicos:"",
+                Afrontar_Problemas:""                
                
             }
         }
@@ -64,7 +102,13 @@ export default {
                         Direccion: this.candidatos.Direccion,
                         Numero_Contacto:this.candidatos.Numero_Contacto,
                         Email: this.candidatos.Email,
-                        Edad: this.candidatos.Edad  
+                        Edad: this.candidatos.Edad,
+                        Has_Tenido_Mascotas:this.candidatos.Has_Tenido_Mascotas,
+                        Seras_Responsable:this.candidatos.Seras_Responsable,
+                        Tienes_Espacio:this.candidatos.Tienes_Espacio,
+                        Tienes_Tiempo:this.candidatos.Tienes_Tiempo,
+                        Recursos_Economicos:this.candidatos.Recursos_Economicos,
+                        Afrontar_Problemas:this.candidatos.Afrontar_Problemas   
                     }
                     
                     this.$emit('completedCandidatoIn', dataCandidatoIn)
@@ -103,6 +147,23 @@ export default {
         flex-direction: column;
         justify-content: center;
         align-items: center;
+    }
+    .DatosPersonales{
+        display: flex;
+        flex-direction: column;
+        margin: 10;
+
+    }
+    .Complementarios{
+        display: flex;
+        flex-direction: column;
+        margin: 10;
+
+    }
+    .Encuesta{
+        display: flex;
+        flex-direction: row;
+
     }
 
     .candidatoIn_user h2{
