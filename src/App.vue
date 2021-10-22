9c66f21e7fa55f22b43a97d5d774e282ed967470
Plozada <plozada>
2021-10-22 08:36:20 -0500

    make it better

--- src/App.vue
+++ src/App.vue
@@ -117,7 +117,7 @@ export default {
     },
     completedSignUp: function (data) {
       alert("Registro Exitoso");
-      this.completedLogIn(data);
+      
     },
     completedMascotaIn: function (data) {
       alert("Mascota registrada con éxito");
